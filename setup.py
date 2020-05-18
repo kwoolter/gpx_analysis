4f6f313553c73cb35ac025dbd3493778995f8629
kwoolter <keith.woolterton@gmail.com>
2020-05-18 10:27:13 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gpx_analysis',
+    version='',
+    packages=['gpx_analysis'],
+    url='',
+    license='',
+    author='kwoolter',
+    author_email='',
+    description='',
+    install_requires = ['pandas', 'gpxpy']
+)
